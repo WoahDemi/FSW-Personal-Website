49749753a5997906eb5e707ffe6e4eae54a608ab
Demi <oluwademiladejobi@pursuit.org>
2021-03-22 18:06:10 -0400

    sans dubugger

--- websiteTwo.js
+++ websiteTwo.js
@@ -3,13 +3,11 @@ const board = document.getElementById("activity-holder")
 const getActivity = async () =>{
     let getData = await axios.get("http://www.boredapi.com/api/activity?type=recreational")
     let getActivity = getData.data.activity
-    debugger
     let activityHolder = document.createElement("li")
     activityHolder.textContent = getActivity
     activityHolder.classList.add("activities")
     board.appendChild(activityHolder)
-    getData.data.activity
-    debugger
+    getData.data.activity 
 }
 button.addEventListener("click", getActivity)
 
